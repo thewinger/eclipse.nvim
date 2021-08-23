acfe3ba4f71b0b60d52dd332f87bc519915595f2
thewinger <winger.87@gmail.com>
2021-08-23 15:54:06 +0200

    Adds compatibility with lualine

--- colors/eclipse.vim
+++ colors/eclipse.vim
@@ -18,4 +18,5 @@ lua package.loaded['lush_theme.eclipse'] = nil
 
 " include our theme file and pass it to lush to apply
 lua require('lush')(require('lush_theme.eclipse'))
+lua require('lualine.themes.eclipse')
 
--- lua/lualine/themes/eclipse.lua
+++ lua/lualine/themes/eclipse.lua
@@ -3,32 +3,32 @@ local colors = require('colors')
 --LuaFormatter on
 return {
   normal = {
-    a = {bg = colors.magenta, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.bg2, fg = colors.bg3},
-    c = {bg = colors.bg1, fg = colors.bg3},
+    a = {bg = colors.fg0, fg = colors.black, gui = colors.bold},
+    b = {bg = colors.bg2, fg = colors.fg1},
+    c = {bg = colors.bg1, fg = colors.fg1},
   },
   insert = {
-    a = {bg = colors.blue, fg = colors.black, gui = 'bold'},
+    a = {bg = colors.blue, fg = colors.black, gui = colors.bold},
     b = {bg = colors.bg2, fg = colors.white},
     c = {bg = colors.bg1, fg = colors.white}
   },
   visual = {
-    a = {bg = colors.yellow, fg = colors.black, gui = 'bold'},
+    a = {bg = colors.yellow, fg = colors.black, gui = colors.bold},
     b = {bg = colors.bg2, fg = colors.white},
     c = {bg = colors.bg1, fg = colors.white}
   },
   replace = {
-    a = {bg = colors.red, fg = colors.black, gui = 'bold'},
+    a = {bg = colors.red, fg = colors.black, gui = colors.bold},
     b = {bg = colors.bg2, fg = colors.white},
-    c = {bg = colrs.bg1, fg = colors.white}
+    c = {bg = colors.bg1, fg = colors.white}
   },
   command = {
-    a = {bg = colors.green, fg = colors.black, gui = 'bold'},
+    a = {bg = colors.green, fg = colors.black, gui = colors.bold},
     b = {bg = colors.bg2, fg = colors.white},
     c = {bg = colors.bg1, fg = colors.white}
   },
   inactive = {
-    a = {bg = colors.bg0, fg = colors.bg2, gui = 'bold'},
+    a = {bg = colors.bg0, fg = colors.bg2, gui = colors.bold},
     b = {bg = colors.bg0, fg = colors.bg2},
     c = {bg = colors.bg0, fg = colors.bg2}
   }
